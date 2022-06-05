bb561abb26a2249e32c45e5d455affc15eaa22e1
Jiashu <3117894278@qq.com>
2022-06-05 10:00:40 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -14,4 +14,8 @@ sudo !!                                         # run last command as root
 echo 3 | sudo tee brightness                    # Each command in a pipeline is executed in its own subshell  
                                                 # Gotcha！sudo echo 3 > brightness 
                                                 # https://effective-shell.com/part-2-core-skills/thinking-in-pipelines/
-
+\                                               # Escape Character
+''                                              # preserves the literal value of each character within the quotes. 
+""                                              # preserves the literal value of all characters except '$', '`', '\'
+chmod 754                                       # change file mode 4(r) + 2(w) + 1(x)
+foo=bar                                         # to assign a variable
